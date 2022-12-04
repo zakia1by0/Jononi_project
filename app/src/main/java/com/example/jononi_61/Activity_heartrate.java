bae40755b9a7d72cfbbac5d2d86390a4f303c073
zakia1by0 <zakiatamannanaora@gmail.com>
2022-12-04 20:41:48 +0600

    First Commit

---
+++ app/src/main/java/com/example/jononi_61/Activity_heartrate.java
@@ -0,0 +1,15 @@
+
+package com.example.jononi_61;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_heartrate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heartrate);
+    }
+}
